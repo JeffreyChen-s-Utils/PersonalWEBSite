05c7d2ba76c0e39ff6a7f523532f89b20fc32ba4
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-07-16 23:03:27 +0800

    Add gevent

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return ""
+    return "HELLO NGINX"
 
 
 if __name__ == "__main__":
-    app.run(port=8888)
+    http_server = WSGIServer(("127.0.0.1", 8888), app)
+    http_server.serve_forever()
